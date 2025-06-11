02fe4bd060618cedf934272ed18f022c8f08d2ed
Willman <willmansatria5@gmail.com>
2025-06-11 13:58:27 +0700

    updated4

--- exercise/day  1/controlFlow/src/Main.java
+++ exercise/day  1/controlFlow/src/Main.java
@@ -2,26 +2,37 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 2;
-        if (x < 2) {
-            System.out.println("Kurang dari 2");
-        } else if (x == 2) {
-            System.out.println("Sama dengan 2");
-        } else if (x > 2) {
-            System.out.println("Lebih dari 2");
+//        int x = 2;
+//        if (x < 2) {
+//            System.out.println("Kurang dari 2");
+//        } else if (x == 2) {
+//            System.out.println("Sama dengan 2");
+//        } else if (x > 2) {
+//            System.out.println("Lebih dari 2");
+//        }
+//
+//        String role = "admin";
+//        switch (role) {
+//            case "admin":
+//                System.out.println("Diijinkan");
+//                break;
+//            case "user":
+//                System.out.println("Tidak Diijinkan");
+//                break;
+//            default:
+//                System.out.println("Tidak Diijinkan");
+//                break;
+
+        for(int i = 1; i<=5; i++){
+            System.out.println(i);
         }
 
-        String role = "admin";
-        switch (role) {
-            case "admin":
-                System.out.println("Diijinkan");
-                break;
-            case "user":
-                System.out.println("Tidak Diijinkan");
-                break;
-            default:
-                System.out.println("Tidak Diijinkan");
-                break;
+        int i = 6;
+
+        while(i<=10){
+            System.out.println(i);
+            i++;
         }
+        
     }
 }
