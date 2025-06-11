412668b1ac8a5b43dc1e5a1b47789887731143fe
Willman <willmansatria5@gmail.com>
2025-06-11 11:59:55 +0700

    updated2

--- exercise/day  1/teshelloWorld/src/Main.java
+++ exercise/day  1/teshelloWorld/src/Main.java
@@ -5,17 +5,32 @@ public class Main {
 //        System.out.printf("Hello World!!");
 //
 
-        int age = 25;
-        double price = 3.14;
-        char grade = 'B';
-        boolean isJavaFun = false;
-        String name = "Willman";
-        int[] numbers = {1, 5, 7, 9};
+//        int age = 25;
+//        double price = 3.14;
+//        char grade = 'B';
+//        boolean isJavaFun = false;
+//        String name = "Willman";
+//        int[] numbers = {1, 5, 7, 9};
+//
+//
+//        System.out.println("Nama: "+name+"\nUmur: " + age);
+//        System.out.printf("Nama: %s Umur: %s",name,age);
+//        System.out.println(grade);
+//        System.out.println(isJavaFun);
 
-        System.out.println("Nama: "+name+"\nUmur: " + age);
-        System.out.printf("Nama: %s Umur: %s",name,age);
-        System.out.println(grade);
-        System.out.println(isJavaFun);
+        int multiple = 30 * 4;
+        int min = 9 - 5;
+        boolean result = multiple>min;
+        boolean result2 = 6<10;
+        boolean result3 = result2 && (2<5);
+        boolean result4 = !result2;
+
+        System.out.println(multiple);
+        System.out.println(min);
+        System.out.println(result);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
 
     }
 }
