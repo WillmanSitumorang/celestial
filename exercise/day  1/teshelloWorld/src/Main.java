78cb9f04bfec02fa4d9414e9ff26617fecf46333
Willman <willmansatria5@gmail.com>
2025-06-11 11:50:08 +0700

    updated

--- exercise/day  1/teshelloWorld/src/Main.java
+++ exercise/day  1/teshelloWorld/src/Main.java
@@ -2,7 +2,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello World!!");
+//        System.out.printf("Hello World!!");
+//
 
-            }
+        int age = 25;
+        double price = 3.14;
+        char grade = 'B';
+        boolean isJavaFun = false;
+        String name = "Willman";
+        int[] numbers = {1, 5, 7, 9};
+
+        System.out.println("Nama: "+name+"\nUmur: " + age);
+        System.out.printf("Nama: %s Umur: %s",name,age);
+        System.out.println(grade);
+        System.out.println(isJavaFun);
+
+    }
 }
