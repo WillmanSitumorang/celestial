3b839c541c140190a084fccf5bf2197e9b4b3e4e
Willman <willmansatria5@gmail.com>
2025-06-11 14:34:48 +0700

    updated5

--- exercise/day  1/controlFlow/src/Main.java
+++ exercise/day  1/controlFlow/src/Main.java
@@ -2,6 +2,20 @@
 public class Main {
     public static void main(String[] args) {
 
+        for(int i = 2; i<=20; i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+
+        int i = 5;
+        int total = 1;
+        while(i>=1){
+            total *= i;
+            i--;
+        }
+        System.out.println(total);
+
 //        int x = 2;
 //        if (x < 2) {
 //            System.out.println("Kurang dari 2");
@@ -23,20 +37,22 @@ public class Main {
 //                System.out.println("Tidak Diijinkan");
 //                break;
 
-        for(int i = 1; i<=5; i++){
-            System.out.println(i);
-        }
+//        for(int i = 1; i<=5; i++){
+//            System.out.println(i);
+//        }
+//
+//        int i = 6;
+//
+//        while(i<=10){
+//            System.out.println(i);
+//            i++;
+//        }
+//
+//        do{
+//            System.out.println(i);
+//            i--;
+//        }while (i>16);
 
-        int i = 6;
 
-        while(i<=10){
-            System.out.println(i);
-            i++;
-        }
-
-        do{
-            System.out.println(i);
-            i++;
-        }while (i<16);
     }
 }
